e33540efce3144e96814570cf495873d7b1a87b1
BrijPurohit <purohit.brijmohan@gmail.com>
2015-02-08 12:25:31 +0530

    new files added
    added python code

---
+++ format_use_in_python.py
@@ -0,0 +1,5 @@
+__author__ = 'Brij'
+date = '8 feb'
+name = 'Brij'
+print ('{} wrote this example on {}'.format(name,date))
+print ('What is your name ?... {}'.format(name))
---
+++ print_in_python.py
@@ -0,0 +1,2 @@
+from __future__ import print_function
+print ("In Python 3.0, print became a function. You need to include parenthesis now.")
